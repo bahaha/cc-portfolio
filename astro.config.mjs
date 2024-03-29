3381dff1db76bbcebab62db18191e1b54fdfb09f
Cheng Che <clementcheng56@gmail.com>
2024-03-29 23:20:41 +0800

    initial frontend frameworks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import aws from "astro-sst";
+import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
+import analogjsangular from "@analogjs/astro-angular";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: aws(),
+  integrations: [tailwind(), vue(), svelte(), react(), analogjsangular()]
 });
